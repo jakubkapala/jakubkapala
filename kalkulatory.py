c4541d418e4778046d5b6d8e2726ab4c9b70ef61
Jakub Kapała <jakub.kapala@gmail.com>
2017-04-11 19:25:32 +0200

    funkcja z pomocą gotowa do reviwe

--- kalkulatory.py
+++ kalkulatory.py
@@ -4,4 +4,8 @@ def dodawanie():
 	b = int(input('podaj pierwsza liczbe'))
 	print(a+b)
 	
+def get_help():
+	print ('to jest prosty program kalkulatora. Wprowadz dwie iczby i zatwierdz enterem')	
+
+get_help()
 dodawanie()
